844d5ebb5fd45237c0cc2d012a79d7b01bb4cab8
Jerry Wonder <jerrycul2001@gmail.com>
2024-08-28 23:40:55 +0100

    fix: debug static files issues

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 /* eslint-disable node/no-unpublished-require */
-import app from './index';
+import app from "./index";
 
-const port = process.env['PORT'];
+const port = process.env["PORT"];
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`App started. Listening on port ${port}`);
